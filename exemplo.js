436a6b33f1c7dbd384e47ab7354ab167be006c8e
laurelzxy <53730508814@info.local>
2024-05-02 17:00:07 -0300

    feat: Exemplo

---
+++ exemplo.js
@@ -0,0 +1,31 @@
+// Escreva um programa que solicite o nome do usuario e lhe de boas vindas,
+// exibindo uma mensagem pelo console.
+
+// prompt - recebe uma entrada
+// let nome = prompt("Olá, digite seu nome:")
+
+// // Contatenação
+// console.log ("Seja bem vindo "+ nome+ " !!!");
+
+let pontos = 100
+
+// parseInt() - Transforma o que esta entre () em numero interio
+// let bonus = parseInt( prompt ('quantos pontos voce deseja somar') )
+
+// Adição - num1 + num2
+// subtracao - num1 - num2
+// multiplicação - num1 * num2
+// divisão - num1 / num2
+
+console.log(pontos + bonus)
+console.log(pontuacaoTotal)
+
+// parseFloat () - Transforma o que esta entre () em um numero com , (quebrado)
+let notaAluno = parseFloat (prompt("Digite a nota do aluno: "))
+
+console.log (notaAluno);
+ 
+
+
+
+
