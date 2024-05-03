c945a02c6e2251c65fb698f886cf2d1b5b2a61b9
laurelzxy <53730508814@info.local>
2024-05-03 16:33:55 -0300

    feat: Exercicio 6

---
+++ ex6.js
@@ -0,0 +1,13 @@
+// Escreva um programa que solicita o ano de nascimento de uma pessoa e verifica se ela é maior
+// de 18 anos. Exiba uma mensagem indicando se a pessoa é maior de idade ou não.
+
+let data = parseFloat (prompt("Digite sua data de nascimento:"))
+
+let idade = (2024 - data)
+
+if (data>18) {
+    console.log("Voce é maior de idade")
+}
+ else {
+    console.log("Voce não é maior de idade")
+ }
