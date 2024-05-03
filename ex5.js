24a7e7e15a0b5cb34c7b8dba72bb42689c120bf7
laurelzxy <53730508814@info.local>
2024-05-03 16:21:52 -0300

    feat: Exercicio 5

---
+++ ex5.js
@@ -0,0 +1,14 @@
+let numero1 = prompt ("Digite o primeiro numero ")
+let numero2 = prompt("Digite o segundo numero ")
+let numero3 = prompt("Digite o terceiro numero ")
+
+if(numero1 < numero2 && numero2 < numero3) {
+    console.log ("A sequencia esta em ordem Crescente")
+
+}
+
+
+else {
+    console.log("Os números não estão em ordem Crescente.");
+}
+
