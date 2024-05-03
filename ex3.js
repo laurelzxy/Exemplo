38790e747238a13b959dc00cfa79c33640859d4b
laurelzxy <53730508814@info.local>
2024-05-03 15:28:16 -0300

    Feat: exercicio 3

---
+++ ex3.js
@@ -0,0 +1,16 @@
+// Faça um programa que receba um número do usuário e informe se este número é par ou
+// ímpar.
+
+let numero = prompt ("Digite um numero ")
+    if(numero % 2 === 0) {
+    console.log(  "Numero é Par.");
+ } 
+
+    else if(numero % 2 === 1) {
+        console.log("Numero é Impar.")
+
+    }
+    else {
+        console.log("Valor não indentificado.")
+    }
+ 
