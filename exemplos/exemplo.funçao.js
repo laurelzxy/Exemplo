c33ef372ecc5cd76d9ecfd38a8029e4e57490326
laurelzxy <53730508814@info.local>
2024-05-09 16:48:09 -0300

    Feat: exemplo função

---
+++ exemplos/exemplo.funçao.js
@@ -0,0 +1,42 @@
+// // // // alert("Hellow world")
+
+// // // function bemVindo(nome){
+// // //     console.log("Bem vindo " + nome);
+// // // }
+
+// // // let retorno = bemVindo("Tadeu")
+
+// // function somar(num1, num2){
+// //     console.log(num1 + num2);
+// // }
+
+// function mostrarSituacaoAluno() {
+//     let nota1 = parseFloat(prompt("Digite a primeira nota"))
+//     let nota2 = parseFloat(prompt("Digite a segunda nota"))
+//     let nota3 = parseFloat(prompt("Digite a terceira nota"))
+
+//     let somaNotas = nota1 + nota2 + nota3
+
+//     let media = somaNotas / 3
+
+//     console.log(media);
+// }
+
+// nome
+// let nome = "Thiago"
+
+// // apelido
+// let apelido= "Thigas"
+
+let pessoa1 = {
+    nome: "Anabelle",
+    apelido: "belle"
+}
+let pessoa2 = {
+    nome: "Agatha",
+    apelido: "tata"
+}
+let pessoa3 = {
+    nome: "yasmin",
+    apelido: "yas"
+}
